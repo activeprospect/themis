18d0f99b101ef2e61db4d71fcf0441dc271bd536
Henrique Laki <138539381+henriquelakiap@users.noreply.github.com>
2024-06-18 12:20:18 -0300

    Move email format test from lc-api export test (#3)
    * Move email format test from lc-api export test

--- test/pattern.js
+++ test/pattern.js
@@ -36,4 +36,81 @@ describe('Issue #3: Crashing when pattern is incorrect in ref(erenced) schema', 
     validator(valid_item, 'kitchensink').valid.should.be.true;
   });
 
+  it('should validate emails correctly', function() {
+    var schemas = [
+      {
+        "id": "types",
+        "$schema": "http://json-schema.org/draft-04/schema#",
+        "definitions": {
+          "email": {
+            "type": "string",
+            "format": "email"
+          }
+        }
+      },
+      {
+        "id": "kitchensink",
+        "$schema": "http://json-schema.org/draft-04/schema#",
+        "type": "object",
+        "properties": {
+          "email": { "$ref": "types#/definitions/email" }
+        }
+      }
+    ];
+
+    var validator = Themis.validator(schemas);
+
+    var valid_emails = [
+      "simple@example.com",
+      "very.common@example.com",
+      "disposable.style.email.with+symbol@example.com",
+      "other.email-with-dash@example.com",
+      "x@example.com",
+      "example-indeed@strange-example.com",
+      "user@domain.aero",
+      "user@domain.arpa",
+      "user@domain.biz",
+      "user@domain.com",
+      "user@domain.coop",
+      "user@domain.edu",
+      "user@domain.gov",
+      "user@domain.info",
+      "user@domain.int",
+      "user@domain.life",
+      "user@domain.mil",
+      "user@domain.museum",
+      "user@domain.name",
+      "user@domain.net",
+      "user@domain.org",
+      "user@domain.pro",
+      "user@domain.travel",
+      "user@domain.mobi",
+      "user@domain.xx"
+    ];
+
+    var invalid_emails = [
+      "plainaddress",
+      "@missingusername.com",
+      "username@.com",
+      "username@.com.",
+      "username@com",
+      "username@.com.com",
+      ".username@yahoo.com",
+      "username@yahoo.com.",
+      "username@yahoo..com",
+      "username@-yahoo.com",
+      "user@domain.toolongtld" // Invalid TLD
+    ];
+
+    valid_emails.forEach(email => {
+      var valid_item = { "email": email };
+      expect(validator(valid_item, 'kitchensink').valid).to.be.true;
+    });
+
+    invalid_emails.forEach(email => {
+      var invalid_item = { "email": email };
+      expect(validator(invalid_item, 'kitchensink').valid).to.be.false;
+    });
+  });
+
 });
